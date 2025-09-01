ab4b1231de9f7f169b94790cd086234faa8bf4c8
ANANTH4004 <balamuruganananth@gmail.com>
2025-09-01 06:17:34 -0700

    jwt token init

---
+++ src/utility/jwtToken.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+
+const accessTokenSecret = process.env.ACCESS_TOKEN_SECRET || 'accessTokenSecret'
+const refreshTokenSecret =
+  process.env.REFRESH_TOKEN_SECRET || 'refreshAccessToken'
+
+export const accessToken = (userId: string) => {
+  return jwt.sign({ userId }, accessTokenSecret, { expiresIn: '15m' })
+}
+
+export const refreshToken = (userId: string) => {
+  return jwt.sign({ userId }, refreshTokenSecret, { expiresIn: '7d' })
+}
+
+export const verifyAccessToken = (token: string) => {
+  return jwt.verify(token, accessTokenSecret)
+}
+
+export const verifyRefreshToken = (token: string) => {
+  return jwt.verify(token, refreshTokenSecret)
+}
